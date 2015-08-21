a6ba0af57078486836944b51569e11692a602117
lightsocks <damnhe@gmail.com>
2015-08-21 13:57:00 +0800

    update

--- main.go
+++ main.go
@@ -14,15 +14,16 @@ func main() {
 	var configFile string
 	flag.StringVar(&configFile, "-c", "config.json", "specify config file")
 	myconfig, err := parseConfig(configFile)
+	if err != nil {
+		log.Println("parse config error:", err)
+	}
 	flag.StringVar(&config.ServerIp, "-s", myconfig.ServerIp, "server address")
 	flag.IntVar(&config.ServerPort, "-p", myconfig.ServerPort, "server port")
 	flag.StringVar(&config.Password, "-k", myconfig.Password, "password")
 	flag.StringVar(&config.Method, "-m", myconfig.Method, "encryption method, default: aes-128-cfb")
 	flag.StringVar(&config.LocalIp, "-b", myconfig.LocalIp, "local address")
 	flag.IntVar(&config.LocalPort, "-l", myconfig.LocalPort, "local port")
-	if err != nil {
-		log.Println("parse config error:", err)
-	}
+
 	var help string
 	flag.StringVar(&help, "-h", "1", "help")
 	flag.Parse()
